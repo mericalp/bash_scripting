3e098ed2063292f6f0c25e3fb3f1f2283a4bebbb
mericalp <83503845+mericalp@users.noreply.github.com>
2024-01-05 23:06:48 +0300

    1 2

---
+++ exercises/exercise1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##!/bin/bash -x = -x tüm komutları görmemizi, hatamızı bulmamızı sağlar.
+
+<<LINUXHINT_MESSAGE  #TOPLU YORUM SATIRI İÇİNE ALIR BU KALIP <<.... ....
+echo -e "one\ttwo\tthree\tfour"  #\t tab
+echo -e "one\ntwo\nthree\nfour"  #\n newline
+echo "one"
+echo "two"
+echo "three"
+LINUXHINT_MESSAGE
+echo "welcome to Joe's bbq"
+#echo "my favorite movie is \"Limitless\"" > ./file.txt
---
+++ exercises/exercise2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# text="kabuk programlama eglencelidir"
+# echo $text
+
+# text=$(hostname)
+# echo "bu script şu makinede çalişiyor : ${text}"
+
+# if [ -f /etc/shadow ]
+# then
+#     echo "shadow parolalari etkin"
+#     if [ -w /etc/shadow ]
+#     then
+#         echo "yazma yetkisi var"
+#     else
+#         echo "yazma yetkisi yok"
+#     fi
+# else
+#     echo "dosya yok"
+# fi
+
