f3728ba80d0eda11a481daadd5a4b7dddb036575
mericalp <meric.winn.alp@gmail.com>
2024-03-29 11:03:35 +0300

    48

---
+++ python/day48.py
@@ -0,0 +1,33 @@
+# Write a function called search_binary that searches for the 
+# number 22 in the following list and returns its index. The 
+# function should take two parameters, the list and the item that 
+# is being searched for. Use binary search (iterative Method).
+# list1 = [12, 34, 56, 78, 98, 22, 45, 13]
+
+def search_binary(list1, x):
+    low = 0
+    high = len(list1) - 1
+    mid_index = 0
+    while low <= high:
+        mid_index = (high + low) // 2
+        if list1[mid_index] == x:
+            return mid_index
+        # if an element is lower than the middle index
+        # then search the lower part of the list
+        elif list1[mid_index] > x:
+            high = mid_index - 1
+        # if an element is higher than the middle index
+        # then search the upper part of the list
+        elif list1[mid_index] < x:
+            low = mid_index + 1
+    # if an element is not found, return -1
+    return 'no element'
+list1 = [12, 34, 56, 78, 98, 22, 45, 13]
+list1 = sorted(list1)
+number = 22
+
+results = search_binary(list1, number)
+if results == 'no element':
+ print('Element is not in the list')
+else:
+ print(f'The element index is {results}')
